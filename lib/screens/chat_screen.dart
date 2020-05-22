ba5cab7a5343e38b7e40f24892f236e4a1555ce0
Carlos Alvarez Moreno <ceam1710@gmail.com>
2020-05-22 16:10:56 -0500

    Adding data

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,23 +5,31 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder<Object>(
+      body: StreamBuilder(
         stream: Firestore.instance
             .collection('chats/f3WsJD29bYgbE9fiGCES/messages')
             .snapshots(),
         builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final documents = snapshot.data.documents;
           return ListView.builder(
-            itemCount: snapshot.data.documents.lenght,
+            itemCount: documents.lenght,
             itemBuilder: (ctx, index) => Container(
               padding: EdgeInsets.all(8),
-              child: Text('This Works'),
+              child: Text(documents[index]['text']),
             ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/f3WsJD29bYgbE9fiGCES/messages')
+              .add({'text': 'This was added by the button'});
+        },
       ),
     );
   }
